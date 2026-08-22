d92736a583d47b6c51c25440b417e23cd4bcdf10
Balaji Narasimhan <balajinarasimhan@Balajis-MBP.lan>
2026-08-22 17:34:33 -0500

    Add LangGraph support workflow and guardrails

--- rag.py
+++ rag.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 import json
 import re
 from pathlib import Path
